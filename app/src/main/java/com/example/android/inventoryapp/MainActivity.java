5dd9f9249fd36f92a0b1abd3d78ba63127eb4ab0
Rif7 <rifraf92@gmail.com>
2018-06-16 23:22:54 +0200

    Created Simple GUI for testing

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.inventoryapp;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.android.inventoryapp.data.InventoryContract;
 import com.example.android.inventoryapp.data.InventoryDbHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
         mDbHelper = new InventoryDbHelper(this);
         logDbPath();
+        updateList();
     }
+
+    // Temporary methods for testing
 
     private void logDbPath() {
         if (mDbHelper!= null) {
@@ -31,4 +38,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void addData(View view) {
+        InventoryUtils.insertInventory(mDbHelper, new Inventory());
+        updateList();
+    }
+
+
+    public void resetData(View view) {
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        String query = "DELETE FROM " + InventoryContract.InventoryEntry.TABLE_NAME;
+        db.execSQL(query);
+        updateList();
+    }
+
+    private void updateList() {
+        TextView textView =  (TextView) findViewById(R.id.db_content);
+        StringCursorParser parser = new StringCursorParser();
+        InventoryUtils.queryInventory(mDbHelper, parser);
+        textView.setText(parser.getParsedQuerry());
+    }
 }
