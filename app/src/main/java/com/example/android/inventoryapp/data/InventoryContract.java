e43fbe7f342ae271742e099df6d87c3642e605ca
Rif7 <rifraf92@gmail.com>
2018-06-16 16:12:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -0,0 +1,55 @@
+package com.example.android.inventoryapp.data;
+
+import android.provider.BaseColumns;
+
+public class InventoryContract {
+
+    private InventoryContract() {}
+
+    /**
+     * Inner class that defines constant values for the inventories database table.
+     * Each entry in the table represents a single inventory.
+     */
+    public static final class InventoryEntry implements BaseColumns {
+
+        /**
+         * Name of database table for inventory
+         */
+        public final static String TABLE_NAME = "inventories";
+
+        /**
+         * Unique ID number for the inventory (only for use in the database table).
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Product Name.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME = "product_name";
+
+        /**
+         * Price in cents.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRICE = "price";
+
+        /**
+         * Quantity.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_QUANTITY = "quantity";
+
+        /**
+         * Supplier Phone Number.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+    }
+}
