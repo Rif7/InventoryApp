cf5cae4e4943016995d5018a8a2265e2747eaa4e
Rif7 <rifraf92@gmail.com>
2018-07-07 00:26:16 +0200

    Add fields validation, save and delete Inventory

--- app/src/main/java/com/example/android/inventoryapp/Inventory.java
+++ app/src/main/java/com/example/android/inventoryapp/Inventory.java
@@ -29,6 +29,10 @@ class Inventory {
         return price;
     }
 
+    public float getFloatPrice() {
+        return ((float) price)/100;
+    }
+
     public Integer getQuantity() {
         return quantity;
     }
@@ -42,6 +46,9 @@ class Inventory {
     }
 
     public void setProductName(String productName) {
+        if (productName == null) {
+            throw new IllegalArgumentException("Product Name is not valid");
+        }
         this.productName = productName;
     }
 
@@ -49,15 +56,51 @@ class Inventory {
         this.price = price;
     }
 
-    public void setQuantity(Integer quantity) {
+    public void setPrice(String price) {
+        try {
+            float realPrice = Float.valueOf(price);
+            this.price = (int) Math.floor(realPrice*100);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Quantity is not a Number");
+        }
+    }
+
+    public void setQuantity(Integer quantity) throws IllegalArgumentException {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative");
+        }
         this.quantity = quantity;
     }
 
+    public void setQuantity(String quantity) throws IllegalArgumentException {
+        int number;
+        try {
+            number = Integer.valueOf(quantity);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Quantity is not a Natural Number");
+        }
+        setQuantity(number);
+    }
+
+    public void incrementQuantity() throws IllegalArgumentException {
+        setQuantity(quantity + 1);
+    }
+
+    public void decrementQuantity() throws IllegalArgumentException {
+        setQuantity(quantity - 1);
+    }
+
     public void setSupplierName(String supplierName) {
+        if (supplierName == null) {
+            throw new IllegalArgumentException("Supplier Name is not valid");
+        }
         this.supplierName = supplierName;
     }
 
     public void setSupplierPhoneNumber(String supplierPhoneNumber) {
+        if (supplierPhoneNumber == null || supplierPhoneNumber.equals("")) {
+            throw new IllegalArgumentException("Supplier Phone Number is not valid");
+        }
         this.supplierPhoneNumber = supplierPhoneNumber;
     }
 
@@ -66,6 +109,6 @@ class Inventory {
         price = 39900;
         quantity = 5;
         supplierName = "NOKIA";
-        supplierPhoneNumber = null;
+        supplierPhoneNumber = "444 2222 666";
     }
 }
--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.inventoryapp;
 
+import java.lang.Math;
+import java.security.InvalidParameterException;
+
 import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
@@ -8,9 +11,13 @@ import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InventoryActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor>{
@@ -18,6 +25,8 @@ public class InventoryActivity extends AppCompatActivity implements
     private static final int EXISTING_INVENTORY_LOADER = 0;
 
     private Uri currentInventoryUri;
+    private Inventory inventory;
+    private boolean inventoryHasChanged = false;
 
     private EditText nameEditText;
     private EditText priceEditText;
@@ -25,28 +34,167 @@ public class InventoryActivity extends AppCompatActivity implements
     private EditText suplierNameEditText;
     private EditText suplierPhoneEditText;
 
-    private Button QuantityMinusButton;
-    private Button QuantityPlusButton;
+    private Button quantityMinusButton;
+    private Button quantityPlusButton;
+
+    private Button deleteButton;
+    private Button saveButton;
+
+    private TextWatcher textWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
         initFieldsViews();
+
+        if (currentInventoryUri == null) {
+            setTitle("Add Product");
+        } else {
+            getLoaderManager().initLoader(EXISTING_INVENTORY_LOADER, null, this);
+            createTextWatcher();
+            inventoryHasChanged = false;
+        }
     }
 
     private void initFieldsViews() {
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         currentInventoryUri = intent.getData();
 
         nameEditText = (EditText) findViewById(R.id.inventory_name);
         priceEditText = (EditText) findViewById(R.id.inventory_price);
         quantityEditText = (EditText) findViewById(R.id.inventory_quantity);
+        quantityEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    try {
+                        inventory.setQuantity(quantityEditText.getText().toString());
+                    } catch (IllegalArgumentException e) {
+                        showToast(e.getMessage());
+                        quantityEditText.setText(String.valueOf(inventory.getQuantity()));
+                    }
+                }
+            }
+        });
+
         suplierNameEditText = (EditText) findViewById(R.id.inventory_supplier_name);
         suplierPhoneEditText = (EditText) findViewById(R.id.inventory_supplier_phone);
 
-        QuantityMinusButton = (Button) findViewById(R.id.inventory_quantity_minus_btn);
-        QuantityPlusButton = (Button) findViewById(R.id.inventory_quantity_plus_but);
+        quantityMinusButton = (Button) findViewById(R.id.inventory_quantity_minus_btn);
+        quantityMinusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    inventory.decrementQuantity();
+                    inventoryHasChanged = true;
+                    quantityEditText.setText(String.valueOf(inventory.getQuantity()));
+                } catch (IllegalArgumentException e) {
+                    showToast(e.getMessage());
+                }
+            }
+        });
+
+        quantityPlusButton = (Button) findViewById(R.id.inventory_quantity_plus_but);
+        quantityPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    inventory.incrementQuantity();
+                    inventoryHasChanged = true;
+                    quantityEditText.setText(String.valueOf(inventory.getQuantity()));
+                } catch (IllegalArgumentException e) {
+                    showToast(e.getMessage());
+                }
+            }
+        });
+
+        deleteButton = (Button) findViewById(R.id.inventory_delete_btn);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentInventoryUri != null) {
+                    delete();
+                }
+            }
+        });
+
+        saveButton = (Button) findViewById(R.id.inventory_save_btn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentInventoryUri != null) {
+                    update();
+                }
+            }
+        });
+
+    }
+
+    private void delete() {
+        if (currentInventoryUri != null) {
+            int rowsDeleted = getContentResolver().delete(currentInventoryUri, null, null);
+            String productName = inventory.getProductName();
+
+            if (rowsDeleted == 0) {
+                Toast.makeText(this, "Failed to delete " + productName,
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Toast.makeText(this, "Deleted " + productName,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void update() {
+        try {
+            inventory.setProductName(nameEditText.getText().toString());
+            inventory.setPrice(priceEditText.getText().toString());
+            inventory.setQuantity(quantityEditText.getText().toString());
+            inventory.setSupplierName(suplierNameEditText.getText().toString());
+            inventory.setSupplierPhoneNumber(suplierPhoneEditText.getText().toString());
+
+            int rowsAffected = getContentResolver().update(currentInventoryUri,
+                    InventoryUtils.getContentValuesFromInventory(inventory),
+                    null, null);
+            if (rowsAffected == 0) {
+                throw new Exception("Failed to update Inventory");
+            }
+            finish();
+        } catch (Exception e) {
+            showToast(e.getMessage());
+        }
+    }
+
+    private void createTextWatcher() {
+        textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                inventoryHasChanged = true;
+            }
+        };
+
+        nameEditText.addTextChangedListener(textWatcher);
+        priceEditText.addTextChangedListener(textWatcher);
+        quantityEditText.addTextChangedListener(textWatcher);
+        suplierNameEditText.addTextChangedListener(textWatcher);
+        suplierPhoneEditText.addTextChangedListener(textWatcher);
+    }
+
+    private void showToast(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     @Override
@@ -67,16 +215,16 @@ public class InventoryActivity extends AppCompatActivity implements
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        Inventory inv = InventoryUtils.getInventoryFromSingleRowCursor(cursor);
-        if (inv != null) {
-            nameEditText.setText(inv.getProductName());
-            priceEditText.setText(String.valueOf(inv.getPrice()));
-            quantityEditText.setText(String.valueOf(inv.getPrice()));
-            if (inv.getSupplierName() != null) {
-                suplierNameEditText.setText(inv.getSupplierName());
+        inventory = InventoryUtils.getInventoryFromSingleRowCursor(cursor);
+        if (inventory != null) {
+            nameEditText.setText(inventory.getProductName());
+            priceEditText.setText(String.valueOf(inventory.getFloatPrice()));
+            quantityEditText.setText(String.valueOf(inventory.getQuantity()));
+            if (inventory.getSupplierName() != null) {
+                suplierNameEditText.setText(inventory.getSupplierName());
             }
-            if (inv.getSupplierPhoneNumber() != null) {
-                suplierPhoneEditText.setText(inv.getSupplierPhoneNumber());
+            if (inventory.getSupplierPhoneNumber() != null) {
+                suplierPhoneEditText.setText(inventory.getSupplierPhoneNumber());
             }
         }
 
@@ -90,4 +238,7 @@ public class InventoryActivity extends AppCompatActivity implements
         suplierNameEditText.setText("");
         suplierPhoneEditText.setText("");
     }
+
 }
+
+
